5710d3192df3c603ad257b0ceeee3537f87ba7df
ChoSeitaku <2374869766@qq.com>
2026-06-12 18:50:25 +0800

    Site updated: 2026-06-12 18:50:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/08/JavaWeb基础面试题精炼/","2026/06/11/Java分布式项目面试题精炼/","2026/06/09/Java后端工程化面试题精炼-上/","2026/05/13/blog-building-guide/","2026/06/07/Java后端宏鼎汇中小厂面经电商供应链/","2026/06/10/Java后端工程化面试题精炼-下/","2026/05/12/hello-world/"];function toRandomPost(){
+var posts=["2026/06/08/JavaWeb基础面试题精炼/","2026/06/11/Java分布式项目面试题精炼/","2026/06/09/Java后端工程化面试题精炼-上/","2026/05/13/blog-building-guide/","2026/06/07/Java后端宏鼎汇中小厂面经电商供应链/","2026/06/10/Java后端工程化面试题精炼-下/","2026/05/12/hello-world/","2026/06/12/Java基础常见面试题之简答题2025-7_最新/","2025/05/14/Java基础常见面试题精炼-一/","2025/05/16/Java基础常见面试题精炼-三/","2025/05/15/Java基础常见面试题精炼-二/","2025/05/17/Java基础常见面试题精炼-四/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
